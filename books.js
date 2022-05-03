9bbe40b2e266cb8a982455db330b987ccdca1815
AliMDhanani <alidhanani2000@gmail.com>
2022-05-03 20:39:30 +0100

    book sortby filter original price and ratings

--- books.js
+++ books.js
@@ -1,32 +1,59 @@
-function renderBooks() {
+function renderBooks(filter) {
   const booksWrapper = document.querySelector(".books");
 
   const books = getBooks();
-  console.log(books);
-  booksWrapper.innerHTML =
-  `<div class="book">
+
+  if(filter === 'LOW_TO_HIGH') {
+    const filteredBooks = books.sort((a, b) => a.originalPrice - b.originalPrice);
+  }
+  else if (filter === 'HIGH_TO_LOW') {
+    const filteredBooks = books.sort((a, b) => b.originalPrice - a.originalPrice);
+  }
+  else if (filter === 'RATING') {
+    const filteredBooks = books.sort((a, b) => b.rating - a.rating);
+  }
+
+  const booksHtml = books.map((book) => {
+    return `<div class="book">
     <figure class="book__img--wrapper">
-      <img class="book__img" src="${books[0].url}" alt="" />
+      <img class="book__img" src="${book.url}" alt="" />
     </figure>
-    <div class="book__title">${books[0].title}</div>
+    <div class="book__title">${book.title}</div>
     <div class="book__ratings">
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star-half-alt"></i>
+      ${ratingsHTML(book.rating)}
     </div>
     <div class="book__price">
-      <span class="book__price--normal">${books[0].originalPrice}</span> ${books[0].salePrice}
+      <span>£${book.originalPrice.toFixed(2)}</span>
     </div>
   </div>`
+  }).join("");
+
+  // console.log(booksHtml)
+  
+  booksWrapper.innerHTML = booksHtml;
 }
+
+function ratingsHTML(rating) {
+  let ratingHTML = '';
+  for (let i = 0; i < Math.floor(rating); i++) {
+    ratingHTML += `<i class="fas fa-star"></i>\n`
+  }
+  if (!Number.isInteger(rating)) {
+    ratingHTML += `<i class="fas fa-star-half-alt"></i>\n`
+  }
+  
+  return ratingHTML
+}
+
+function filterBooks(event) {
+  renderBooks(event.target.value);
+};
 
 setTimeout(() => {
   renderBooks();
 });
 
-// FAKE DATA
+//Book Data
 function getBooks() {
   return [
     {
@@ -59,7 +86,7 @@ function getBooks() {
       url: "assets/book-1.jpeg",
       originalPrice: 44,
       salePrice: 19,
-      rating: 4.5,
+      rating: 3.5,
     },
     {
       id: 5,
@@ -91,7 +118,7 @@ function getBooks() {
       url: "assets/book-5.jpeg",
       originalPrice: 38,
       salePrice: 17.95,
-      rating: 4.5,
+      rating: 3.5,
     },
     {
       id: 9,
@@ -99,7 +126,7 @@ function getBooks() {
       url: "assets/book-6.jpeg",
       originalPrice: 35,
       salePrice: null,
-      rating: 4,
+      rating: 2.5,
     },
     {
       id: 10,
@@ -115,7 +142,7 @@ function getBooks() {
       url: "assets/book-8.jpeg",
       originalPrice: 30,
       salePrice: null,
-      rating: 4.5,
+      rating: 3.5,
     },
   ];
 }
